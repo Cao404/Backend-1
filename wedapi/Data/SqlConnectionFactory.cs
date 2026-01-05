354153f31383f5884fd065bf238e00c38a9e239e
Cao404 <Vuhuucao789@gmmail.com>
2026-01-05 21:18:36 +0700

    ket noi

---
+++ wedapi/Data/SqlConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace SellerHub.Api.Data;
+
+public interface ISqlConnectionFactory
+{
+    SqlConnection Create();
+}
+
+public class SqlConnectionFactory : ISqlConnectionFactory
+{
+    private readonly IConfiguration _config;
+    public SqlConnectionFactory(IConfiguration config) => _config = config;
+
+    public SqlConnection Create()
+    {
+        var cs = _config.GetConnectionString("Default");
+        return new SqlConnection(cs);
+    }
+}
