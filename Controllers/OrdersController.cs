f432c64ab9c6445e7c0ae12d5a8de4d1aaa1be3e
duyhuy2005 <duyhuy2812@gmail.com>
2026-01-05 19:17:02 +0700

    ok

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using User.Api.Common;
+using User.Api.Dtos;
+using User.Api.Repositories;
+
+namespace User.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderRepository _repo;
+        public OrdersController(IOrderRepository repo) => _repo = repo;
+
+        // đặt hàng từ giỏ
+        [HttpPost]
+        public IActionResult Place([FromBody] PlaceOrderRequest req)
+        {
+            var orderId = _repo.PlaceOrderFromCart(req.MaKhachHang, req.DiaChiGiao, req.GhiChu);
+            return Ok(ApiResponse<object>.Ok(new { MaDonHang = orderId }, "Đặt hàng thành công"));
+        }
+
+        // danh sách đơn theo khách
+        [HttpGet]
+        public IActionResult List([FromQuery] int maKhachHang)
+        {
+            var list = _repo.GetOrdersByCustomer(maKhachHang);
+            return Ok(ApiResponse<object>.Ok(list));
+        }
