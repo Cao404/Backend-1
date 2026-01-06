0ffc583752b6073f4aef4e43b76771a29d137ca1
Cao404 <Vuhuucao789@gmmail.com>
2026-01-06 22:03:21 +0700

    lam trang thai ng ban

--- wedapi/Model/SellerAppStatus.cs
+++ wedapi/Model/SellerAppStatus.cs
@@ -1,6 +1,11 @@
-﻿namespace wedapi.Model
+﻿namespace SellerHub.Api.Models;
+
+public enum SellerAppStatus
 {
-    public class SellerAppStatus
-    {
-    }
+    Pending = 0,
+    Approved = 1,
+    Rejected = 2,
+    Suspended = 3,
+    Locked = 4,
+    Submitted = 5
 }
