60134db9292eedde55852607605f3e63f32b3536
duyhuy2005 <duyhuy2812@gmail.com>
2026-01-06 14:14:32 +0700

    update Program

--- source/repos/wedduyhuy/Program.cs
+++ source/repos/wedduyhuy/Program.cs
@@ -1,4 +1,5 @@
 using wedduyhuy.Repositories;
+using wedduyhuy.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,10 @@ builder.Services.AddControllers();
 
 // Register repositories
 builder.Services.AddScoped<IDanhMucRepository, DanhMucRepository>();
+builder.Services.AddScoped<ISanPhamRepository, SanPhamRepository>();
+builder.Services.AddScoped<IKhachHangRepository, KhachHangRepository>();
+builder.Services.AddScoped<IDonHangRepository, DonHangRepository>();
+builder.Services.AddScoped<IGioHangRepository, GioHangRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
