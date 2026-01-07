15c719bf4051984d14d066df85134630174e4b34
duyhuy2005 <duyhuy2812@gmail.com>
2026-01-07 09:41:55 +0700

    DonHangRepository

--- source/repos/wedduyhuy/Repositories/DanhMucRepository.cs
+++ source/repos/wedduyhuy/Repositories/DanhMucRepository.cs
@@ -1,6 +1,6 @@
 using Microsoft.Data.SqlClient;
-using System.Data;
 using wedduyhuy.DTOs;
+using wedduyhuy.Repositories.Interfaces;
 
 namespace wedduyhuy.Repositories
 {
@@ -10,181 +10,132 @@ namespace wedduyhuy.Repositories
 
         public DanhMucRepository(IConfiguration configuration)
         {
-            _connectionString = configuration.GetConnectionString("DefaultConnection") 
-                ?? throw new ArgumentNullException("Connection string not found");
+            _connectionString = configuration.GetConnectionString("DefaultConnection")!;
         }
 
         public async Task<IEnumerable<DanhMucDto>> GetAllAsync()
         {
-            var danhMucs = new List<DanhMucDto>();
-            
-            using var connection = new SqlConnection(_connectionString);
-            await connection.OpenAsync();
-            
-            var sql = @"
-                SELECT dm.DanhMucID, dm.MaDanhMuc, dm.TenDanhMuc, dm.DanhMucChaID, 
-                       dm.TrangThai, dm.NgayTao, dm.NgayCapNhat,
-                       cha.TenDanhMuc as TenDanhMucCha
-                FROM dbo.DanhMuc dm
-                LEFT JOIN dbo.DanhMuc cha ON dm.DanhMucChaID = cha.DanhMucID
-                WHERE dm.IsDeleted = 0
-                ORDER BY dm.TenDanhMuc";
-            
-            using var command = new SqlCommand(sql, connection);
-            using var reader = await command.ExecuteReaderAsync();
-            
+            var list = new List<DanhMucDto>();
+            using var conn = new SqlConnection(_connectionString);
+            await conn.OpenAsync();
+
+            var sql = @"SELECT dm.DanhMucID, dm.MaDanhMuc, dm.TenDanhMuc, dm.DanhMucChaID, 
+                        dm.TrangThai, dm.NgayTao, dm.NgayCapNhat, cha.TenDanhMuc as TenDanhMucCha
+                        FROM dbo.DanhMuc dm
+                        LEFT JOIN dbo.DanhMuc cha ON dm.DanhMucChaID = cha.DanhMucID
+                        WHERE dm.IsDeleted = 0 ORDER BY dm.TenDanhMuc";
+
+            using var cmd = new SqlCommand(sql, conn);
+            using var reader = await cmd.ExecuteReaderAsync();
             while (await reader.ReadAsync())
             {
-                danhMucs.Add(new DanhMucDto
-                {
-                    DanhMucID = reader.GetInt32("DanhMucID"),
-                    MaDanhMuc = reader.IsDBNull("MaDanhMuc") ? null : reader.GetString("MaDanhMuc"),
-                    TenDanhMuc = reader.GetString("TenDanhMuc"),
-                    DanhMucChaID = reader.IsDBNull("DanhMucChaID") ? null : reader.GetInt32("DanhMucChaID"),
-                    TenDanhMucCha = reader.IsDBNull("TenDanhMucCha") ? null : reader.GetString("TenDanhMucCha"),
-                    TrangThai = reader.GetBoolean("TrangThai"),
-                    NgayTao = reader.GetDateTime("NgayTao"),
-                    NgayCapNhat = reader.IsDBNull("NgayCapNhat") ? null : reader.GetDateTime("NgayCapNhat")
-                });
+                list.Add(MapToDto(reader));
             }
-            
-            return danhMucs;
+            return list;
         }
 
         public async Task<DanhMucDto?> GetByIdAsync(int id)
         {
-            using var connection = new SqlConnection(_connectionString);
-            await connection.OpenAsync();
-            
-            var sql = @"
-                SELECT dm.DanhMucID, dm.MaDanhMuc, dm.TenDanhMuc, dm.DanhMucChaID, 
-                       dm.TrangThai, dm.NgayTao, dm.NgayCapNhat,
-                       cha.TenDanhMuc as TenDanhMucCha
-                FROM dbo.DanhMuc dm
-                LEFT JOIN dbo.DanhMuc cha ON dm.DanhMucChaID = cha.DanhMucID
-                WHERE dm.DanhMucID = @Id AND dm.IsDeleted = 0";
-            
-            using var command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("@Id", id);
-            
-            using var reader = await command.ExecuteReaderAsync();
-            
-            if (await reader.ReadAsync())
-            {
-                return new DanhMucDto
-                {
-                    DanhMucID = reader.GetInt32("DanhMucID"),
-                    MaDanhMuc = reader.IsDBNull("MaDanhMuc") ? null : reader.GetString("MaDanhMuc"),
-                    TenDanhMuc = reader.GetString("TenDanhMuc"),
-                    DanhMucChaID = reader.IsDBNull("DanhMucChaID") ? null : reader.GetInt32("DanhMucChaID"),
-                    TenDanhMucCha = reader.IsDBNull("TenDanhMucCha") ? null : reader.GetString("TenDanhMucCha"),
-                    TrangThai = reader.GetBoolean("TrangThai"),
-                    NgayTao = reader.GetDateTime("NgayTao"),
-                    NgayCapNhat = reader.IsDBNull("NgayCapNhat") ? null : reader.GetDateTime("NgayCapNhat")
-                };
-            }
-            
+            using var conn = new SqlConnection(_connectionString);
+            await conn.OpenAsync();
+
+            var sql = @"SELECT dm.DanhMucID, dm.MaDanhMuc, dm.TenDanhMuc, dm.DanhMucChaID, 
+                        dm.TrangThai, dm.NgayTao, dm.NgayCapNhat, cha.TenDanhMuc as TenDanhMucCha
+                        FROM dbo.DanhMuc dm
+                        LEFT JOIN dbo.DanhMuc cha ON dm.DanhMucChaID = cha.DanhMucID
+                        WHERE dm.DanhMucID = @Id AND dm.IsDeleted = 0";
+
+            using var cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@Id", id);
+            using var reader = await cmd.ExecuteReaderAsync();
+            if (await reader.ReadAsync()) return MapToDto(reader);
             return null;
         }
 
         public async Task<int> CreateAsync(CreateDanhMucDto dto)
         {
-            using var connection = new SqlConnection(_connectionString);
-            await connection.OpenAsync();
-            
-            var sql = @"
-                INSERT INTO dbo.DanhMuc (MaDanhMuc, TenDanhMuc, DanhMucChaID)
-                OUTPUT INSERTED.DanhMucID
-                VALUES (@MaDanhMuc, @TenDanhMuc, @DanhMucChaID)";
-            
-            using var command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("@MaDanhMuc", (object?)dto.MaDanhMuc ?? DBNull.Value);
-            command.Parameters.AddWithValue("@TenDanhMuc", dto.TenDanhMuc);
-            command.Parameters.AddWithValue("@DanhMucChaID", (object?)dto.DanhMucChaID ?? DBNull.Value);
-            
-            var result = await command.ExecuteScalarAsync();
-            return Convert.ToInt32(result);
+            using var conn = new SqlConnection(_connectionString);
+            await conn.OpenAsync();
+
+            var sql = @"INSERT INTO dbo.DanhMuc (MaDanhMuc, TenDanhMuc, DanhMucChaID)
+                        OUTPUT INSERTED.DanhMucID
+                        VALUES (@MaDanhMuc, @TenDanhMuc, @DanhMucChaID)";
+
+            using var cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@MaDanhMuc", (object?)dto.MaDanhMuc ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@TenDanhMuc", dto.TenDanhMuc);
+            cmd.Parameters.AddWithValue("@DanhMucChaID", (object?)dto.DanhMucChaID ?? DBNull.Value);
+
+            return Convert.ToInt32(await cmd.ExecuteScalarAsync());
         }
 
         public async Task<bool> UpdateAsync(int id, UpdateDanhMucDto dto)
         {
-            using var connection = new SqlConnection(_connectionString);
-            await connection.OpenAsync();
-            
-            var sql = @"
-                UPDATE dbo.DanhMuc 
-                SET MaDanhMuc = @MaDanhMuc, 
-                    TenDanhMuc = @TenDanhMuc, 
-                    DanhMucChaID = @DanhMucChaID,
-                    TrangThai = @TrangThai,
-                    NgayCapNhat = SYSDATETIME()
-                WHERE DanhMucID = @Id AND IsDeleted = 0";
-            
-            using var command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("@Id", id);
-            command.Parameters.AddWithValue("@MaDanhMuc", (object?)dto.MaDanhMuc ?? DBNull.Value);
-            command.Parameters.AddWithValue("@TenDanhMuc", dto.TenDanhMuc);
-            command.Parameters.AddWithValue("@DanhMucChaID", (object?)dto.DanhMucChaID ?? DBNull.Value);
-            command.Parameters.AddWithValue("@TrangThai", dto.TrangThai);
-            
-            var rowsAffected = await command.ExecuteNonQueryAsync();
-            return rowsAffected > 0;
+            using var conn = new SqlConnection(_connectionString);
+            await conn.OpenAsync();
+
+            var sql = @"UPDATE dbo.DanhMuc SET MaDanhMuc = @MaDanhMuc, TenDanhMuc = @TenDanhMuc, 
+                        DanhMucChaID = @DanhMucChaID, TrangThai = @TrangThai, NgayCapNhat = SYSDATETIME()
+                        WHERE DanhMucID = @Id AND IsDeleted = 0";
+
+            using var cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@Id", id);
+            cmd.Parameters.AddWithValue("@MaDanhMuc", (object?)dto.MaDanhMuc ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@TenDanhMuc", dto.TenDanhMuc);
+            cmd.Parameters.AddWithValue("@DanhMucChaID", (object?)dto.DanhMucChaID ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@TrangThai", dto.TrangThai);
+
+            return await cmd.ExecuteNonQueryAsync() > 0;
         }
 
         public async Task<bool> DeleteAsync(int id)
         {
-            using var connection = new SqlConnection(_connectionString);
-            await connection.OpenAsync();
-            
-            var sql = @"
-                UPDATE dbo.DanhMuc 
-                SET IsDeleted = 1, NgayCapNhat = SYSDATETIME()
-                WHERE DanhMucID = @Id";
-            
-            using var command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("@Id", id);
-            
-            var rowsAffected = await command.ExecuteNonQueryAsync();
-            return rowsAffected > 0;
+            using var conn = new SqlConnection(_connectionString);
+            await conn.OpenAsync();
+
+            var sql = "UPDATE dbo.DanhMuc SET IsDeleted = 1, NgayCapNhat = SYSDATETIME() WHERE DanhMucID = @Id";
+            using var cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            return await cmd.ExecuteNonQueryAsync() > 0;
         }
 
         public async Task<IEnumerable<DanhMucDto>> GetByParentIdAsync(int? parentId)
         {
-            var danhMucs = new List<DanhMucDto>();
-            
-            using var connection = new SqlConnection(_connectionString);
-            await connection.OpenAsync();
-            
-            var sql = @"
-                SELECT dm.DanhMucID, dm.MaDanhMuc, dm.TenDanhMuc, dm.DanhMucChaID, 
-                       dm.TrangThai, dm.NgayTao, dm.NgayCapNhat,
-                       cha.TenDanhMuc as TenDanhMucCha
-                FROM dbo.DanhMuc dm
-                LEFT JOIN dbo.DanhMuc cha ON dm.DanhMucChaID = cha.DanhMucID
-                WHERE dm.IsDeleted = 0 AND (@ParentId IS NULL AND dm.DanhMucChaID IS NULL OR dm.DanhMucChaID = @ParentId)
-                ORDER BY dm.TenDanhMuc";
-            
-            using var command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("@ParentId", (object?)parentId ?? DBNull.Value);
-            
-            using var reader = await command.ExecuteReaderAsync();
-            
+            var list = new List<DanhMucDto>();
+            using var conn = new SqlConnection(_connectionString);
+            await conn.OpenAsync();
+
+            var sql = @"SELECT dm.DanhMucID, dm.MaDanhMuc, dm.TenDanhMuc, dm.DanhMucChaID, 
+                        dm.TrangThai, dm.NgayTao, dm.NgayCapNhat, cha.TenDanhMuc as TenDanhMucCha
+                        FROM dbo.DanhMuc dm
+                        LEFT JOIN dbo.DanhMuc cha ON dm.DanhMucChaID = cha.DanhMucID
+                        WHERE dm.IsDeleted = 0 AND (@ParentId IS NULL AND dm.DanhMucChaID IS NULL OR dm.DanhMucChaID = @ParentId)
+                        ORDER BY dm.TenDanhMuc";
+
+            using var cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@ParentId", (object?)parentId ?? DBNull.Value);
+            using var reader = await cmd.ExecuteReaderAsync();
             while (await reader.ReadAsync())
             {
-                danhMucs.Add(new DanhMucDto
-                {
-                    DanhMucID = reader.GetInt32("DanhMucID"),
-                    MaDanhMuc = reader.IsDBNull("MaDanhMuc") ? null : reader.GetString("MaDanhMuc"),
-                    TenDanhMuc = reader.GetString("TenDanhMuc"),
-                    DanhMucChaID = reader.IsDBNull("DanhMucChaID") ? null : reader.GetInt32("DanhMucChaID"),
-                    TenDanhMucCha = reader.IsDBNull("TenDanhMucCha") ? null : reader.GetString("TenDanhMucCha"),
-                    TrangThai = reader.GetBoolean("TrangThai"),
-                    NgayTao = reader.GetDateTime("NgayTao"),
-                    NgayCapNhat = reader.IsDBNull("NgayCapNhat") ? null : reader.GetDateTime("NgayCapNhat")
-                });
+                list.Add(MapToDto(reader));
             }
-            
-            return danhMucs;
+            return list;
+        }
+
+        private static DanhMucDto MapToDto(SqlDataReader r)
+        {
+            return new DanhMucDto
+            {
+                DanhMucID = r.GetInt32(r.GetOrdinal("DanhMucID")),
+                MaDanhMuc = r.IsDBNull(r.GetOrdinal("MaDanhMuc")) ? null : r.GetString(r.GetOrdinal("MaDanhMuc")),
+                TenDanhMuc = r.GetString(r.GetOrdinal("TenDanhMuc")),
+                DanhMucChaID = r.IsDBNull(r.GetOrdinal("DanhMucChaID")) ? null : r.GetInt32(r.GetOrdinal("DanhMucChaID")),
+                TenDanhMucCha = r.IsDBNull(r.GetOrdinal("TenDanhMucCha")) ? null : r.GetString(r.GetOrdinal("TenDanhMucCha")),
+                TrangThai = r.GetBoolean(r.GetOrdinal("TrangThai")),
+                NgayTao = r.GetDateTime(r.GetOrdinal("NgayTao")),
+                NgayCapNhat = r.IsDBNull(r.GetOrdinal("NgayCapNhat")) ? null : r.GetDateTime(r.GetOrdinal("NgayCapNhat"))
+            };
         }
     }
 }
