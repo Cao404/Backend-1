d7292720132a791e75e3ed7f79a63a73f255b1d9
huy2005okbro-netizen <huy2005okbro@gmail.com>
2026-01-08 00:27:44 +0700

    Dtos khach hang

--- Dtos/KhachHangDtos.cs
+++ Dtos/KhachHangDtos.cs
@@ -1,6 +1,13 @@
-﻿namespace apitranhuy.Dtos
+﻿namespace SellerHub.Api.Dtos;
+
+public class KhachHangDto
 {
-    public class KhachHangDtos
-    {
-    }
+    public int MaKhachHang { get; set; }
+    public string HoTen { get; set; } = "";
+    public string? Email { get; set; }
+    public string SoDienThoai { get; set; } = "";
+    public string? DiaChi { get; set; }
+    public string Loai { get; set; } = "thuong";
+
+   
 }
