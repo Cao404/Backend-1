1b0c5ddf9d32d87109631b9c9317df828a11dd69
Cao404 <Vuhuucao789@gmmail.com>
2026-01-07 21:36:57 +0700

    lam them ng dung

---
+++ wedapi/Controllers/UsersRepository.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SellerHub.Api.Controllers
+{
+    public class UsersRepository
+    {
+        internal void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool Unlock(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
