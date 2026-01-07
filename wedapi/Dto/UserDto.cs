7577d30ad9307b7eb0d4e4a6f2006e28212fe8e4
Cao404 <Vuhuucao789@gmmail.com>
2026-01-07 21:39:06 +0700

    them dto ng dung

---
+++ wedapi/Dto/UserDto.cs
@@ -0,0 +1,9 @@
+﻿public class UserDto
+{
+    public int Id { get; set; }
+    public required string FullName { get; set; }
+    public required string Email { get; set; }
+    public string? Phone { get; set; }
+    public required string Role { get; set; }
+    public required string Status { get; set; }
+}
