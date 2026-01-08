162b7d385ef16b0571268f86e65e2dcaef529675
huy2005okbro-netizen <huy2005okbro@gmail.com>
2026-01-08 23:13:39 +0700

    sửa models sp

--- Models/CreateSanPhamRequest.cs
+++ Models/CreateSanPhamRequest.cs
@@ -1,6 +1,11 @@
-﻿namespace apitranhuy.Model
+﻿namespace SellerHub.Api.Models.Requests;
+
+public sealed class CreateSanPhamRequest
 {
-    public class CreateSanPhamRequest
-    {
-    }
+    public string SKU { get; set; } = "";
+    public string TenSanPham { get; set; } = "";
+    public int MaDanhMuc { get; set; }
+    public decimal GiaBan { get; set; }
+    public int Kho { get; set; }
+  
 }
