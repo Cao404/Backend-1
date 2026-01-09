39f40963ecfe910a1be968a71240a48286149de2
huy2005okbro-netizen <huy2005okbro@gmail.com>
2026-01-09 23:27:30 +0700

    Hoand chỉnh CreateSanPhamRequest

--- Models/CreateSanPhamRequest.cs
+++ Models/CreateSanPhamRequest.cs
@@ -7,5 +7,6 @@ public sealed class CreateSanPhamRequest
     public int MaDanhMuc { get; set; }
     public decimal GiaBan { get; set; }
     public int Kho { get; set; }
-  
+    public int DaBan { get; set; }
+    public bool TrangThai { get; set; } = true; // mặc định đang bán
 }
