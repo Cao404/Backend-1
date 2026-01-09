9b8ee9227876412b2beed44f1bd718ed5812cf08
Cao404 <Vuhuucao789@gmmail.com>
2026-01-10 00:10:20 +0700

    update

---
+++ wedapi/Data/SellerApplicationsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using SellerHub.Api.Models;
+
+namespace SellerHub.Api.Repositories;
+
+public class SellerApplicationsRepository
+{
+    private readonly string _cs;
+
+    public SellerApplicationsRepository(IConfiguration config)
+    {
+        _cs = config.GetConnectionString("Default")
+              ?? throw new InvalidOperationException("Missing ConnectionStrings:Default");
+    }
+}
